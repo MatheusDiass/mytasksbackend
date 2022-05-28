6990ae9044ce02ab61913f65df701d4af130475a
Matheus Dias <dias.math0@gmail.com>
2022-05-28 13:57:43 -0300

    Criação da request para listar as tarefas de um usuário

--- src/modules/groups/groups.service.js
+++ src/modules/groups/groups.service.js
@@ -1,15 +1,27 @@
 import Group from './group.model.js';
 
 class GroupService {
-  //Adiciona o grupo de tarefas
+  //Adiciona um grupo de tarefas
   async save(group) {
     const { name } = group;
+
     try {
       if (!name) {
         throw { data: 'Parâmetros passados incorretamente!', code: 400 };
       }
 
       await Group.create(group);
+    } catch (error) {
+      throw { data: error, code: 500 };
+    }
+  }
+
+  //Obtém todos os grupos de tarefas
+  async fetchGroups() {
+    try {
+      const groups = await Group.find();
+
+      return groups;
     } catch (error) {
       throw { data: error, code: 500 };
     }
