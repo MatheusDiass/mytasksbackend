2e0cb82c8b02cb1591052ab5eedcc399cf422436
LeonardoLima <leeofr_lima@hotmail.com>
2022-05-30 23:20:18 -0300

    Criação da request para editar uma tarefa

--- src/modules/tasks/tasks.service.js
+++ src/modules/tasks/tasks.service.js
@@ -14,22 +14,41 @@ class TasksService {
       throw { data: error, code: 500 };
     }
   }
-    //Deletar tarefa
-    async deleteTasks(taskId) {
-      try {
-        if (!taskId) {
-          throw { data: 'Parâmetros passados incorretamente!', code: 400 };
-        }
-  
-        const operationInfo = await Task.deleteOne({ _id: taskId });
-  
-        if (!operationInfo.deletedCount) {
-          throw { data: 'tarefa não encontrada!', code: 400 };
-        }
-      } catch (error) {
-        throw { data: error, code: 500 };
+
+  //Deletar tarefa
+  async deleteTasks(taskId) {
+    try {
+      if (!taskId) {
+        throw { data: 'Parâmetros passados incorretamente!', code: 400 };
       }
+
+      const operationInfo = await Task.deleteOne({ _id: taskId });
+
+      if (!operationInfo.deletedCount) {
+        throw { data: 'tarefa não encontrada!', code: 400 };
+      }
+    } catch (error) {
+      throw { data: error, code: 500 };
     }
+  }
+  //Editar tarefa
+  async updatetasks(taskId, task) {
+  
+
+    try {
+      if (!taskId) {
+        throw { data: 'Parâmetros passados incorretamente!', code: 400 };
+      }
+
+      const operationInfo = await task.updateOne({ _id: taskId }, task);
+
+      if (!operationInfo.matchedCount) {
+        throw { data: 'Tarefa não encontrada!', code: 400 };
+      }
+    } catch (error) {
+      throw { data: error, code: 500 };
+    }
+  }
 }
 
 export default new TasksService();
